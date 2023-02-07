807c6857f8c35d902832dd5741c685fa63dc2f23
Johannes Maier <johannes.maier@mailbox.org>
2023-02-07 23:26:00 +0100

    Load a file and move the cursor around

--- src/main.lisp
+++ src/main.lisp
@@ -1,5 +1,5 @@
 (eval-when (:compile-toplevel :execute :load-toplevel)
-  (ql:quickload '(:croatoan-ncurses :slynk)))
+  (ql:quickload '(:croatoan :croatoan-ncurses :slynk)))
 
 (defpackage #:vasuki
   (:use :cl))
@@ -7,35 +7,74 @@
 (in-package #:vasuki)
 
 (defun init-ncurses ()
-  (ncurses:initscr)
-  (ncurses:cbreak)
-  (ncurses:noecho)
-  (ncurses:clear))
+  (let ((scr (ncurses:initscr)))
+    (ncurses:cbreak)
+    (ncurses:noecho)
+    (ncurses:keypad scr t)
+    (ncurses:clear)))
+
+(defclass buffer ()
+  ((content :initarg :content
+            :accessor content)
+   (cursor-x :initform 0
+             :accessor cursor-x)
+   (cursor-y :initform 0
+             :accessor cursor-y)))
+
+(defun make-buffer (content)
+  (make-instance 'buffer :content content))
+
+(defun int->key (k)
+  (cond
+    ((equalp k (char-int #\q))
+     :quit)
+    ((equalp (crt:key-code-to-name k) :left)
+     :left)
+    ((equalp (crt:key-code-to-name k) :right)
+     :right)
+    ((equalp (crt:key-code-to-name k) :up)
+     :up)
+    ((equalp (crt:key-code-to-name k) :down)
+     :down)))
+
+(defun move-cursor (buf dir)
+  (case dir
+    (:left (decf (cursor-x buf)))
+    (:right (incf (cursor-x buf)))
+    (:up (decf (cursor-y buf)))
+    (:down (incf (cursor-y buf)))))
+
+(defun print-buffer (buf)
+  (loop
+    for line in (content buf)
+    for y from 0
+    do (ncurses:mvaddstr y 0 line))
+  (ncurses:move (cursor-y buf) (cursor-x buf))
+  (ncurses:refresh))
+
+(defun load-file (path)
+  (let* ((lines (uiop:read-file-lines path))
+         (buf (make-buffer lines)))
+    (print-buffer buf)
+    buf))
+
+(defun movement? (key)
+  (member key '(:left :right :up :down)))
+
+(defun handle-input (buf)
+  (let ((key (int->key (ncurses:getch))))
+    (when (not (equalp key :quit))
+      (when (movement? key)
+        (move-cursor buf key))
+      (progn
+        (print-buffer buf)
+        (handle-input buf)))))
 
 (defun main ()
   (init-ncurses)
-  ;; draw a Sierpinski triangle: https://www.linuxjournal.com/content/getting-started-ncurses
-  (let* ((maxlines (- ncurses:lines 1))
-         (maxcols (- ncurses:cols 1))
-         (x (list 0 (truncate maxcols 2) maxcols))
-         (y (list 0 maxlines 0))
-         (xi (mod (random 10000) maxcols))
-         (yi (mod (random 10000) maxlines)))
-    (ncurses:mvaddstr (car y) (car x) "0")
-    (ncurses:mvaddstr (cadr y) (cadr x) "1")
-    (ncurses:mvaddstr (caddr y) (caddr x) "2")
-    (ncurses:mvaddstr yi xi ".")
-    (dotimes (i 1000)
-      (progn
-        (let ((index (mod (random 10000) 3)))
-          (setf yi (truncate (+ yi (nth index y)) 2))
-          (setf xi (truncate (+ xi (nth index x)) 2))
-          (ncurses:mvaddstr yi xi "*")
-          (ncurses:refresh))))
-    (ncurses:mvaddstr maxlines 0 "Press any key to quit.")
-    (ncurses:refresh)
-    (ncurses:getch)
-    (ncurses:endwin)))
+  (let ((buf (load-file "flake.nix")))
+    (handle-input buf))
+  (ncurses:endwin))
 
 (slynk:create-server :port 4321
                      :dont-close t)
