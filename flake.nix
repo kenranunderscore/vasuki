d4e3fdbec8be42bc2015f8cd0b50e5276108e928
Johannes Maier <johannes.maier@active-group.de>
2023-02-08 14:10:05 +0100

    Add sbcl to dev environment

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
   outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default =
       let pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in pkgs.mkShell { buildInputs = [ pkgs.ncurses ];
-      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.ncurses ];};
+      in pkgs.mkShell {
+        buildInputs = [ pkgs.ncurses pkgs.sbcl ];
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.ncurses ];
+      };
   };
 }
